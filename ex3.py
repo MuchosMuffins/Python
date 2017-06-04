b18360a4fa6a098d22042c13f5b6404d0c6a8ccd
Michael Bilbang <muffin@localhost.localdomain>
2017-06-04 11:51:22 +0200

    ex3_update

--- ex3.py
+++ ex3.py
@@ -1,10 +1,14 @@
+#Print
 print "I will now count my chickens:"
 
+#We do some math, Punkt vor Strichrechnung
 print "Hens", 25 + 30 / 6
 print "Roosters", 100 - 25 * 3 % 4
 
+
 print "Now I will count the eggs:"
 
+#Punkt vor Strich
 print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
 print "Is it true that 3 + 2 < 5 - 7?"
