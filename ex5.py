ebc31c7fd496c7743559b42be184207444d980c6
Michael Bilbang <muffin@localhost.localdomain>
2017-06-05 22:32:55 +0200

    ex5_update

--- ex5.py
+++ ex5.py
@@ -1,13 +1,13 @@
-my_name = "Michael Bilbang"
-my_age = 28
-my_height = 180
-my_weight = 95
-my_eyes = "Blau"
-my_hair = "Braun"
+name = "Michael Bilbang"
+age = 28
+height = 180
+weight = 95
+eyes = "Blau"
+hair = "Braun"
 
-print "Lass uns ueber %s sprechen." % my_name
-print "Er ist %d cm gross." % my_height
-print "Er ist %d kg schwer." % my_weight
-print "Er hat die Augenfarbe %s und sein Haar ist %s." % (my_eyes, my_hair)
+print "Lass uns ueber %s sprechen." % name
+print "Er ist %d cm gross." % height
+print "Er ist %d kg schwer." % weight
+print "Er hat die Augenfarbe %s und sein Haar ist %s." % (eyes, hair)
 
-print "Wenn ich %d, %d und %d zusammenzaehle, bekomme ich %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+print "Wenn ich %d, %d und %d zusammenzaehle, bekomme ich %d." % (age, height, weight, age + height + weight)
