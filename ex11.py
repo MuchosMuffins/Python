c5780da3a108cefb610f986cce3d78805e10143d
root <root@DESKTOP-Muffin.localdomain>
2020-11-27 21:30:44 +0100

    ex 10 und 11

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print("How old are you in years?", end=' ')
+age = input()
+print("How tall are you in m?", end=' ')
+height = input()
+print("How much do you weigh in kg?", end=' ')
+weight = input()
+
+print(f"So, you're {age} years old, {height} meter tall and {weight} kg heavy.")
